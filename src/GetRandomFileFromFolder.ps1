c68603d7f43a37d8d741f3f54b4d244320aa67db
Prince Biswaranjan <39714581+pbvirus@users.noreply.github.com>
2022-08-22 21:05:41 +0530

    Print total number of files in the folder

--- src/GetRandomFileFromFolder.ps1
+++ src/GetRandomFileFromFolder.ps1
@@ -1,6 +1,15 @@
-$FolderPath = "D:\Downloads\*";
-$Files = Get-ChildItem -Path $FolderPath -Recurse -File;
-$RandomFile = $Files | Get-Random -Count 1;
+$folderPath = "D:\Downloads\*";
+$excludedPaths = @(
+    "D:\Downloads\Marriage Pictures\*.*",
+    "D:\Downloads\OnePlus 5T Backup\2020-06-16\DCIM\Camera\*",
+    "D:\Downloads\Photo prints\Brown",
+    "D:\Downloads\pre wedding",
+    "D:\Downloads\wedding\*.*"
+);
 
-Write-Host($RandomFile.FullName);
-explorer.exe /select,$RandomFile.FullName;
+$files = Get-ChildItem -Path $folderPath -Recurse -File -Exclude $excludedPaths;
+$randomFile = $files | Get-Random -Count 1;
+
+Write-Host("Total Files: " + $files.Length);
+Write-Host($randomFile.FullName);
+explorer.exe /select,$randomFile.FullName;
